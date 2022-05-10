d1eb3f96d32c3c499fd1688b0f8097e2eb1bdfcf
Ivan Shasholka <Ivan_Shasholka@epam.com>
2022-05-10 15:53:47 +0300

    Custom serialization added

---
+++ 10_Serialization/Serialization/CustomSerialization/Cat.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomSerialization
+{
+    [Serializable]
+    public class Cat : ISerializable
+    {
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public int Age { get; set; }
+
+        public Cat(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public Cat()
+        {
+        }
+
+        public Cat(SerializationInfo info, StreamingContext context)
+        {
+            Name = (string) info.GetValue("Name", typeof(string));
+            Age = (int) info.GetValue("Age", typeof(int));
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Name", Name);
+            info.AddValue("Age", Age);
+        }
+
+        public override string ToString()
+        {
+            return Name + " " + Age;
+        }
+    }
+}
---
+++ 10_Serialization/Serialization/CustomSerialization/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+using CustomSerialization;
+
+string filePath = @"..\..\..\CatData.dat";
+
+
+var cat = new Cat("George", 13);
+
+var stream = File.Open(filePath, FileMode.Create);
+
+var bf = new BinaryFormatter();
+
+bf.Serialize(stream, cat);
+
+cat = null;
+stream.Close();
+
+stream = File.Open(filePath, FileMode.Open);
+
+bf = new BinaryFormatter();
+
+cat = (Cat)bf.Deserialize(stream);
+
+stream.Close();
+
+Console.WriteLine(cat);
